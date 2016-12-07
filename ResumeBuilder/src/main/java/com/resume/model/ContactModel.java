f1e84e72910be5e76d199542a28b4eb4aeb69627
akash <akashsonker66@gmail.com>
2016-12-07 12:58:41 +0530

    hi
    hi

--- ResumeBuilder/src/main/java/com/resume/model/ContactModel.java
+++
@@ -1,5 +0,0 @@
-package com.resume.model;
-
-public class ContactModel {
-
-}
--- ResumeBuilder/src/main/java/com/resume/test/Test.java
+++
@@ -1,9 +0,0 @@
-package com.resume.test;
-
-public class Test {
-
-	public static void main(String[] args) {
-		
-	}
-
-}
